909785f30f5c781bd391c2b718d59a93a4f2f863
PrimCarol <primca@esat-alumni.com>
2025-01-10 09:05:22 +0100

    Adds time calculation and fix overflow x on about page.

--- src/components/dataCard/DataCard.js
+++ src/components/dataCard/DataCard.js
@@ -14,6 +14,34 @@ function DataCard({ data }) {
     return `${month.slice(0, 3)}. ${year}`;
   };
 
+  function CalcularDiferenciaFechas(fechaInicio, fechaFinal) {
+    // Asegúrate de que las fechas sean instancias de Date
+    const inicio = new Date(fechaInicio);
+    const final = fechaFinal ? new Date(fechaFinal) : new Date();
+
+    // Obtener los años y meses de cada fecha
+    const anioInicio = inicio.getFullYear();
+    const mesInicio = inicio.getMonth();
+    const anioFinal = final.getFullYear();
+    const mesFinal = final.getMonth();
+
+    // Calcular la diferencia de años y meses
+    let totalAnios = anioFinal - anioInicio;
+    let totalMeses = mesFinal - mesInicio;
+
+    // Ajustar si los meses de la fecha final son menores que los de la fecha inicial
+    if (totalMeses < 0) {
+        totalAnios--;
+        totalMeses += 12;
+    }
+
+     var finalString = "(";
+     if (totalAnios > 0) { finalString += totalAnios + " year" + (totalAnios > 1 ? "s " : " "); }
+     finalString += totalMeses + " month" + (totalMeses > 1 ? "s)" : ")");
+
+     return finalString;
+  }
+
   return (
     <Card className="DataCard_Container">
       <CardContent className="DataCard_Wrapper">
@@ -27,8 +55,7 @@ function DataCard({ data }) {
           <div className="DataCard_HeaderText">
             <Typography
               variant="h4"
-              sx={{ marginLeft: "0px", letterSpacing: "0px" }}
-            >
+              sx={{ marginLeft: "0px", letterSpacing: "0px" }}>
               {data.title}
             </Typography>
             <Typography variant="body1" sx={{ color: "#A3B1BE" }}>
@@ -37,19 +64,9 @@ function DataCard({ data }) {
             <Typography variant="body1" sx={{ color: "#A3B1BE" }}>
               {data.location}
             </Typography>
-              {data.currentlyHere ?
-                (
-                  <Typography variant="body1" sx={{ color: "#A3B1BE" }}>
-                    {formatDate(data.startDate)} - Currenly
-                  </Typography>
-                )
-                :
-                (
-                  <Typography variant = "body1" sx = {{ color: "#A3B1BE" }}>
-                    {formatDate(data.startDate)} - {formatDate(data.endDate)}
-                  </Typography>
-                )
-              }
+             <Typography variant="body1" sx={{ color: "#A3B1BE" }}>
+               {formatDate(data.startDate)} - {data.currentlyHere ? "Currently" : formatDate(data.endDate)} {CalcularDiferenciaFechas(data.startDate, data.currentlyHere ? null : data.endDate) }
+             </Typography>
           </div>
         </div>
 
